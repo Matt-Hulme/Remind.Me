351a81fc7a97c3b4c3551ee32209fb62590cdcb3
Matt-Hulme <Mattwhulme@gmail.com>
2023-10-13 21:33:33 -0600

    Textbox started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react' 
 import './App.css'
+import UrlField from './components/UrlField.jsx'
 
 function App() {
 
   return (
     <div className='App'>
+      <div className='AppContainer'>
+        <h1 id="Mainh1">Remind me about this URL</h1>
+        <UrlField />
+      </div>
     </div>
   )
 }
---
+++ src/components/UrlField.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function UrlField() {
+    return (
+        <>
+            <form className="UrlFieldForm">
+                <label for="UrlTextBox">
+                </label>         
+                <input className="UrlTextBox" type="text" name="UrlTextBox">
+                </input>
+            </form>
+        </>
+    )
+}

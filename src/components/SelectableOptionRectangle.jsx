4bcf07f988796aca0401d071daeb587e14e90268
Matt-Hulme <Mattwhulme@gmail.com>
2023-11-08 18:22:21 -0700

    Removing Comments

--- src/components/SelectableOptionRectangle.jsx
+++ src/components/SelectableOptionRectangle.jsx
@@ -4,8 +4,8 @@ export default function SelectableOptionRectangle({ children, onClick }) {
   const [isActive, setActive] = useState(false);
 
   const handleOptionClick = () => {
-    setActive(!isActive); // Toggle the active state
-    onClick(); // Call the provided onClick function
+    setActive(!isActive);
+    onClick(); 
   };
 
   return (

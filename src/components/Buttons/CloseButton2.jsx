21471479955587a74ad9ba738e87b2e3881f268f
Matt-Hulme <Mattwhulme@gmail.com>
2023-11-12 11:37:24 -0700

    .

--- src/components/Buttons/CloseButton2.jsx
+++ src/components/Buttons/CloseButton2.jsx
@@ -3,7 +3,7 @@ import CloseButtonIcon from '../../assets/CloseButtonIcon.svg'
 export default function CloseButton() {
   return (
     <div className="CloseButton2">
-      <img src={CloseButtonIcon} alt="X">
+      <img src={CloseButtonIcon}>
       </img>
     </div>
   )

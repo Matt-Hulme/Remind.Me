c6735493289acd828935ebd89381793d3e6bee99
Matt-Hulme <Mattwhulme@gmail.com>
2023-11-22 12:44:15 -0700

    Re-design Layout Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import './App.css'
+import './AppNew.css'
 import EmailCapturePage from './components/Pages/EmailCapturePage'
 import MainPage from './components/Pages/MainPage.jsx'
+import MainPageNew from './components/Pages/MainPageNew.jsx'
+
 import ConfirmationPage from './components/Pages/ConfirmationPage'
 
 
@@ -8,7 +10,7 @@ export default function App() {
 
   return (
     <div className='App'>
-      <MainPage /> 
+      <MainPageNew /> 
     </div>
   )
 }
---
+++ src/components/Pages/MainPageNew.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import CloseButton from '../Buttons/CloseButton.jsx';
+import UrlField from '../Fields/UrlField.jsx';
+import DateAndTimeSelector from '../DateAndTimeSelector.jsx';
+import NotiTypeSelector from '../NotiTypeSelector.jsx';
+import SaveButton from '../Buttons/SaveButton.jsx';
+
+export default function MainPage() {
+  const [url, setUrl] = useState('');
+  const [notiDate, setNotiDate] = useState('');
+  const [notiTime, setNotiTime] = useState('');
+  const [notiType, setNotiType] = useState('');
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const handleDateChange = (newDate) => {
+    setNotiDate(newDate);
+  };
+
+  const handleTimeChange = (newTime) => {
+    setNotiTime(newTime);
+  };
+
+  const handleNotiTypeChange = (selectedType) => {
+    setNotiType(selectedType);
+  };
+
+  return (
+    <div className='MainPage'>
+      <div className='MainPageContainer'>
+        <CloseButton />
+        <div className='MainPageMainContainer'>
+          <h1 className="Mainh1">Remind me about this URL</h1>
+          <h2 className="MainSubtitle">URL</h2>
+          <UrlField onUrlChange={handleUrlChange} />
+          <DateAndTimeSelector onDateChange={handleDateChange} onTimeChange={handleTimeChange} />
+          <NotiTypeSelector onNotiTypeChange={handleNotiTypeChange} />
+          <SaveButton
+            url={url}
+            notiDate={notiDate}
+            notiTime={notiTime}
+            notifiType={notiType}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
